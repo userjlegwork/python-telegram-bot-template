3046ce89abb716bd26969c85e47599cdd53b6109
José Luis Escobar Gámez <userjlegwork@gmail.com>
2022-07-05 00:29:11 -0400

    feat(commands): Removed wrong access to DB

--- commands/start.py
+++ commands/start.py
@@ -4,7 +4,6 @@ import logging
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from db.connection import DBHandler
 from utils.user import get_user_first_name
 
 logger = logging.getLogger(__name__)
@@ -13,9 +12,6 @@ logger = logging.getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     logger.info("Start command called")
-    DBHandler().execute(
-        "CREATE TABLE test3 (id serial PRIMARY KEY, num integer, data varchar)"
-    )
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=f"Welcome {get_user_first_name(update)}!",  # 1 Handle translate
