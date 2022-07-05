85ce41f59f61dae889c9750b4d54723519745738
José Luis Escobar Gámez <userjlegwork@gmail.com>
2022-07-05 01:00:22 -0400

    fix(command-start): Corrected log

--- commands/start.py
+++ commands/start.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
-    logger.info("Start command called")
+    logger.info("/start command called")
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=f"Welcome {get_user_first_name(update)}!",  # 1 Handle translate
