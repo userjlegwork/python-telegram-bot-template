6623c670d394c0cb824c9ab1375d3e3ff500ffa7
José Luis Escobar Gámez <userjlegwork@gmail.com>
2022-07-05 00:18:32 -0400

    feat(db): Simplified connection management

--- db/connection.py
+++ db/connection.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 class DBHandler:
     """Handle postgresql connection."""
 
-    conn = None
+    pool = None
 
     @classmethod
     def connect_pool(cls) -> None:
@@ -20,43 +20,19 @@ class DBHandler:
 
         logger.info("Connecting to the PostgreSQL database... (pool)")
         try:
-            cls.conn = ThreadedConnectionPool(
+            cls.pool = ThreadedConnectionPool(
                 5,
                 50,
                 host=os.getenv("POSTGRES_HOST", "localhost"),
                 database=os.getenv("POSTGRES_DB", "testbot"),
                 user=os.getenv("POSTGRES_USER", "postgres"),
                 password=os.getenv("POSTGRES_PASSWORD", "postgres"),
-            ).getconn()
-
-            logger.info("PostgreSQL database connected (pool)")
-            with cls.conn.cursor() as cur:
-                cur.execute("SELECT version();")
-                logger.info(
-                    "Connected to PostgreSQL database version: %s", cur.fetchone()
-                )
-        except (psycopg2.DatabaseError) as error:
-            logger.error(error)
-
-        finally:
-            if not cur.closed:
-                cur.close()
-
-    @classmethod
-    def connect(cls) -> None:
-        """Connect to the database."""
-
-        logger.info("Connecting to the PostgreSQL database...")
-        try:
-            cls.conn = psycopg2.connect(
-                host=os.getenv("POSTGRES_HOST", "localhost"),
-                database=os.getenv("POSTGRES_DB", "testbot"),
-                user=os.getenv("POSTGRES_USER", "postgres"),
-                password=os.getenv("POSTGRES_PASSWORD", "postgres"),
             )
 
-            logger.info("PostgreSQL database connected")
-            with cls.conn.cursor() as cur:
+            conn = cls.pool.getconn()
+
+            logger.info("PostgreSQL database connected (pool)")
+            with conn.cursor() as cur:
                 cur.execute("SELECT version();")
                 logger.info(
                     "Connected to PostgreSQL database version: %s", cur.fetchone()
@@ -67,32 +43,21 @@ class DBHandler:
         finally:
             if not cur.closed:
                 cur.close()
+
+            if conn:
+                cls.pool.putconn(conn)
 
     @classmethod
     def execute(cls, query: str, params: Tuple = None) -> List:
         """Execute a query."""
         result = []
-        if cls.conn is None:
-            cls.connect()
+        if cls.pool is None:
+            cls.connect_pool()
 
-        with cls.conn.cursor() as cur:
-            try:
+        with cls.pool.getconn() as conn:
+            with conn.cursor() as cur:
                 cur.execute(query, params)
-                cls.conn.commit()
-                if cur.rowcount > 0:
-                    result = cur.fetchall()
-            except (psycopg2.DatabaseError) as error:
-                logger.error(error)
-            finally:
-                if not cur.closed:
-                    cur.close()
-                cls.close_connection()
+                result = cur.fetchall()
+
+            cls.pool.putconn(conn)
         return result
-
-    @classmethod
-    def close_connection(cls) -> None:
-        """Close the connection."""
-
-        if cls.conn is not None:
-            cls.conn.close()
-            logger.info("PostgreSQL connection closed")
