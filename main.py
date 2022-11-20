15641c24065046a49a85e1f9ad0b6562ff5ad6ef
Jose Luis Escobar Gamez <joseluis@dynabliss.com>
2022-11-20 16:55:15 -0500

    Error handler async

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Logs telegram api errors
-def error(update, context):
+async def error(update, context):
     """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    await logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
